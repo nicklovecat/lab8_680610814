34d76f93a0226fe633ccab1fef1a989746d38e0e
nicklovecat <skykillergamer@gmail.com>
2025-12-19 19:09:29 +0700

    lab8_1 completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -5,16 +5,19 @@ int main() {
     char rank;
     cout << "Input your rank: ";
     cin >> rank;
+
     int lv = 0;
     if (rank == 'S') lv = 5;
     else if (rank == 'A') lv = 4;
     else if (rank == 'B') lv = 3;
     else if (rank == 'C') lv = 2;
     else if (rank == 'D') lv = 1;
+
     if (lv >= 5) cout << "You have received Super Ultra Rare Unit!!!\n";
     if (lv >= 4) cout << "You have received 5 gems.\n";
     if (lv >= 3) cout << "You have received 1 gems.\n";
     if (lv >= 2) cout << "You have received 2000 coins.\n";
     if (lv >= 1) cout << "You have received very KAK items.\n";
+
     return 0;
 }
